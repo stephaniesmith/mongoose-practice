5df773a342c835de64469ebfc276f8be1db8b446
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-17 14:52:36 -0700

    enum and min tests

--- test/Company.test.js
+++ test/Company.test.js
@@ -3,11 +3,16 @@ const Company = require('../lib/models/Company');
 
 describe('Company model', () => {
 
+    const getValidationErrors = validation => {
+        assert.isDefined(validation, 'should give errors');
+        return validation.errors;
+    };
+
     it('valid model', () => {
         const data = {
             name: 'Orc.is',
             description: 'Screenprinting',
-            type: 'Manufacturing',
+            type: 'profit',
             address: {
                 street: '123 Place Street',
                 city: 'Walla Walla',
@@ -26,14 +31,41 @@ describe('Company model', () => {
         assert.isUndefined(company.validateSync());
     });
 
+    it('has default date', () => {
+        const company = new Company({ name: 'ACL' });
+        assert.ok(company.founded);
+        assert.isAtMost(company.founded - Date.now(), 5);
+    });
+
     it('required fields', () => {
         const company = new Company({});
         const validation = company.validateSync();
         assert.isDefined(validation, 'should give errors');
-        const { errors } = validation;
+        const errors = getValidationErrors(company.validateSync());
         assert.equal(Object.keys(errors).length, 3);
         assert.equal(errors.name.kind, 'required');
         assert.equal(errors['address.state'].kind, 'required');
         assert.equal(errors.size.kind, 'required');
+    });
+
+    it('enum, positive size', () => {
+        const company = new Company({
+            name: 'Orc.is',
+            description: 'Screenprinting',
+            type: 'Manufacturing',
+            address: {
+                street: '123 Place Street',
+                city: 'Walla Walla',
+                zip: '97362',
+                state: 'WA'
+            },
+            founded: new Date(),
+            size: 0,
+            isHip: true,
+            keywords: ['screenprinting', 'design', 'small']
+        });
+        const errors = getValidationErrors(company.validateSync());
+        assert.equal(errors['type'].kind, 'enum');
+        assert.equal(errors['size'].kind, 'min');
     });
 });
