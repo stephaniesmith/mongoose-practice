a51fb5d86ddfe4c3f8c275af917a86d6f86bda86
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-17 14:17:15 -0700

    added founded date

--- test/Company.test.js
+++ test/Company.test.js
@@ -14,6 +14,7 @@ describe('Company model', () => {
                 zip: '97362',
                 state: 'WA'
             },
+            founded: new Date(),
             size: 2,
             isHip: true,
             keywords: ['screenprinting', 'design', 'small']
